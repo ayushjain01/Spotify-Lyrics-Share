c2be6a4c01dc301d74aac66a3a3ce0e2391ac09d
Ayush Jain <ayushganna67@gmail.com>
2024-08-22 17:44:42 +0530

    Add metadata

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -2,20 +2,40 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
-
 export const metadata = {
-  title: "lyrics share",
-  description: "share lyrics with custom open graph.",
+  title: "Lyrics Share - Dynamic Open Graph Generation",
+  description:
+    "This project is inspired by the lyrics share feature on Spotify, which allows users to select specific excerpts from a song and share them with friends. The backend generates a custom Open Graph preview image for the selected lyrics, making it easy to share highlighted lyrics visually.",
+  author: "Ayush Jain",
+  openGraph: {
+    title: "Lyrics Share - Dynamic Open Graph Generation",
+    type: "website",
+    locale: "en_IE",
+    description:
+    "This project is inspired by the lyrics share feature on Spotify, which allows users to select specific excerpts from a song and share them with friends. The backend generates a custom Open Graph preview image for the selected lyrics, making it easy to share highlighted lyrics visually.",
+    images: [
+      {
+        url: "",
+        width: 800,
+        height: 600,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Lyrics Share - Dynamic Open Graph Generation",
+    description:
+      "This project is inspired by the lyrics share feature on Spotify, which allows users to select specific excerpts from a song and share them with friends. The backend generates a custom Open Graph preview image for the selected lyrics, making it easy to share highlighted lyrics visually.",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      
       <body>
-        <Navbar/>
+        <Navbar />
         {children}
-        <Footer/>
+        <Footer />
       </body>
     </html>
   );
