b786e4f10a90c8c58e35b86359a2601e49e46f58
Ayush Jain <ayushganna67@gmail.com>
2024-08-22 18:49:14 +0530

    Add cors middleware

--- server/main.py
+++ server/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"], 
+    allow_origins=["http://localhost:3000", "https://spotify-lyrics-share.vercel.app"], 
     allow_credentials=True,
     allow_methods=["*"], 
     allow_headers=["*"],  
